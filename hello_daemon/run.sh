bb8e761e3f7eb25227569379d40ad8134b5234dc
Baul <baul.hoa@v-key.com>
2025-11-15 21:44:43 +0800

    Clean up

--- hello_daemon/run.sh
+++
@@ -1,6 +0,0 @@
-launchctl stop hq.bao.hello_daemo
-launchctl unload /Library/LaunchDaemons/hq.bao.hello_daemon.plist
-launchctl load /Library/LaunchDaemons/hq.bao.hello_daemon.plist
-launchctl start hq.bao.hello_daemo
-# launchctl list
-tail -f /var/log/hello_daemon.log
--- hello_daemon/stop.sh
+++
@@ -1,2 +0,0 @@
-launchctl stop hq.bao.hello_daemo
-launchctl unload /Library/LaunchDaemons/hq.bao.hello_daemon.plist
