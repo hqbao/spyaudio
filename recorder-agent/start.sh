6e60b7e21abc363f50119b2689828e0f0ead8612
Baul <baul.hoa@v-key.com>
2025-11-17 15:37:09 +0800

    Update deployment scripts

--- recorder-agent/start.sh
+++ recorder-agent/start.sh
@@ -4,4 +4,4 @@ launchctl stop hq.bao.recagent
 launchctl unload $JB_DIR/Library/LaunchDaemons/hq.bao.recagent.plist
 launchctl load $JB_DIR/Library/LaunchDaemons/hq.bao.recagent.plist
 launchctl start hq.bao.recagent
-tail -f $JB_DIR/var/log/recagent.log
+# tail -f $JB_DIR/var/log/recagent.log
